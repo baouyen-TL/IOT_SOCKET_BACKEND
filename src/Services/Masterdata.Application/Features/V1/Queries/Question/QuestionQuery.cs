afa6ff75428eb51982655cd0769bbd200907f2b4
vinhvq545 <vinh.vq@isdcorp.vn>
2024-05-29 10:20:21 +0700

    update

--- src/Services/Masterdata.Application/Features/V1/Queries/Question/QuestionQuery.cs
+++ src/Services/Masterdata.Application/Features/V1/Queries/Question/QuestionQuery.cs
@@ -24,8 +24,50 @@ namespace Masterdata.Application.Features.V1.Queries.Question
         public async Task<QuestionResponse> GetListQuestionByTopicId(Guid TopicId)
         {
             var QuestionRes = new QuestionResponse();
+
             var topic = await _context.TopicModels.FirstOrDefaultAsync(x => x.TopicId == TopicId);
             if (topic == null) throw new BadRequestException("TopicId này không tồn tại!");
+
+            // Response Topic
+            QuestionRes.TopicId = TopicId;
+            QuestionRes.TopicName = topic.TopicName;
+
+            var listQuestions = await _context.QuestionModels.Where(x => x.TopicId == TopicId)
+                .Include(x => x.AnswerModels)
+                .ToListAsync();
+
+            // Response ListQuestion
+            var ListQuestionDataRes = new List<QuestionDataResponse>();
+            foreach (var question in listQuestions)
+            {
+                var QuestionDataRes = new QuestionDataResponse
+                {
+                    QuestionId = question.QuestionId,
+                    QuestionName = question.QuestionName,
+                    QuestionTime = question.QuestionTime,
+                };
+
+                // Response List Answer
+                var ListAnswerRes = new List<AnswerResponse>();
+                foreach (var answer in ListQuestionDataRes)
+                {
+                    var answerInfo = question.AnswerModels.Where(x => x.QuestionId == question.QuestionId).FirstOrDefault();
+                    var answerRes = new AnswerResponse
+                    {
+                        AnswerId = answerInfo.AnswerId,
+                        AnswerName = answerInfo.AnswerName,
+                        IsCorrect = answerInfo.IsCorrect,
+                    };
+
+                    ListAnswerRes.Add(answerRes);
+                }
+                // Response List Answer in Question
+                QuestionDataRes.ListAnswerDatas = ListAnswerRes;
+                // Response List Question in Topic
+                ListQuestionDataRes.Add(QuestionDataRes);
+            };
+
+            QuestionRes.ListQuestionDatas = ListQuestionDataRes;
 
             return QuestionRes;
         }
