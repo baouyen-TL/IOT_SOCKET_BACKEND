0496abc4c2019534fe2283e8c70d246d22ce5c90
vinhvq545 <vinh.vq@isdcorp.vn>
2024-05-29 10:23:02 +0700

    update

---
+++ src/Services/Masterdata/Masterdata.API/Controllers/BeginGameController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Responses;
+using Masterdata.Application.Features.V1.Commands.BeginGame;
+using Masterdata.Application.Features.V1.Commands.Question;
+using Masterdata.Application.Features.V1.Queries.Question;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Masterdata.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeginGameController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IQuestionQuery _query;
+
+        public BeginGameController(IMediator mediator, IQuestionQuery query)
+        {
+            _mediator = mediator;
+            _query = query;
+        }
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBeginGame(CreateBeginGameCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(new ApiSuccessResponse
+            {
+                Data = result,
+                Message = "Tạo thành công"
+            });
+        }
+    }
+}
---
+++ src/Services/Masterdata/Masterdata.API/Controllers/SaveAnswerController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Responses;
+using Masterdata.Application.Features.V1.Commands.BeginGame;
+using Masterdata.Application.Features.V1.Commands.SaveAnswer;
+using Masterdata.Application.Features.V1.Queries.Question;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Masterdata.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaveAnswerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IQuestionQuery _query;
+
+        public SaveAnswerController(IMediator mediator, IQuestionQuery query)
+        {
+            _mediator = mediator;
+            _query = query;
+        }
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateBeginGame(CreateSaveAnswerCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(new ApiSuccessResponse
+            {
+                Data = result,
+                Message = "Tạo thành công"
+            });
+        }
+    }
+}
